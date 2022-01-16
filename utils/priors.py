60e6b24649a505063c5cf4ad125275f776545590
AbdullrahmanHMD <manofsteel272@yahoo.com>
2022-01-16 22:26:49 +0300

    Vectorize priors implementation (pair-programmed with @sarieh)

--- utils/priors.py
+++ utils/priors.py
@@ -1,4 +1,8 @@
 import math
+import numpy as np
+import torch
+
+FEATURE_LAYERS = ['feature_1', 'feature_2', 'feature_3', 'feature_4', 'feature_5', 'feature_6']
 
 def priors_specs():
     """
@@ -7,59 +11,62 @@ def priors_specs():
     """
 
     conv1_aratio = [1, 2, 1/2, 3/2]
-    conv_1 = {"fdim": 38, "scale": 0.1, "aratios": conv1_aratio, "num": 4, "total": 5776}  # 38 x 38 x 4 = 5776
+    feature_1 = {"fdim": 38, "scale": 0.1, "aratios": conv1_aratio, "num": 4, "total": 5776}  # 38 x 38 x 4 = 5776
 
     conv2_aratio = [1, 2, 1/2, 3, 1/3, 2.5, 1/2.5]
-    conv_2 = {"fdim": 19, "scale": 0.2, "aratios": conv2_aratio, "num": 7, "total": 2527}  # 19 x 19 x 7 = 2527
+    feature_2 = {"fdim": 19, "scale": 0.2, "aratios": conv2_aratio, "num": 7, "total": 2527}  # 19 x 19 x 7 = 2527
 
     conv3_aratio = [1, 2, 1/2, 3, 1/3, 1/2.5]
-    conv_3 = {"fdim": 10, "scale": 0.38, "aratios": conv3_aratio, "num": 6, "total": 600} # 10 x 10 x 6 = 600 
+    feature_3 = {"fdim": 10, "scale": 0.38, "aratios": conv3_aratio, "num": 6, "total": 600} # 10 x 10 x 6 = 600 
 
     conv4_aratio = [1, 2, 1/2, 3, 1/3, 2.5, 1/2.5]
-    conv_4 = {"fdim": 5, "scale": 0.55, "aratios": conv4_aratio, "num": 7, "total": 175}  # 5 x 5 x 7 = 175
+    feature_4 = {"fdim": 5, "scale": 0.55, "aratios": conv4_aratio, "num": 7, "total": 175}  # 5 x 5 x 7 = 175
 
     conv5_aratio = [1, 2, 1/2, 3, 1/3]
-    conv_5 = {"fdim": 3, "scale": 0.725, "aratios": conv5_aratio, "num": 5, "total": 45} # 3 x 3 x 5 = 45
+    feature_5 = {"fdim": 3, "scale": 0.725, "aratios": conv5_aratio, "num": 5, "total": 45} # 3 x 3 x 5 = 45
 
     conv6_aratio = [1, 2, 1/2, 3, 1/3]
-    conv_6 = {"fdim": 1, "scale": 0.9, "aratios": conv6_aratio, "num": 5, "total": 5}   # 1 x 1 x 5 = 5
+    feature_6 = {"fdim": 1, "scale": 0.9, "aratios": conv6_aratio, "num": 5, "total": 5}   # 1 x 1 x 5 = 5
 
     total_prior_num = 9128 # Total: 9128
 
-    return {'conv_1':conv_1,'conv_2': conv_2,'conv_3': conv_3,'conv_4': conv_4,'conv_5': conv_5,'conv_6': conv_6}, total_prior_num
+    return {'feature_1':feature_1,'feature_2': feature_2,'feature_3': feature_3,'feature_4': feature_4,'feature_5': feature_5,'feature_6': feature_6}, total_prior_num
 
-# --- Helper functions ---------------------------------------------------
+def create_priors():
 
-def prior_dim(scale, aspect_ratio):
-    w = scale * math.sqrt(aspect_ratio)
-    h = scale / math.sqrt(aspect_ratio)
+    specs = priors_specs()[0]
+    all_priors = np.empty((0, 4))
 
-    return w, h
+    for layer in FEATURE_LAYERS:
 
+        fdim = specs[layer]['fdim']
+        aratios = specs[layer]['aratios']
+        scale = specs[layer]['scale']
 
-def create_prior_aux(scale, aspect_ratios):
-    priors = []
+        indecies = np.indices((fdim, fdim)).reshape(2, -1)
 
-    for aratio in aspect_ratios:
-        priors.append(prior_dim(scale, aratio))
+        X = indecies[0, :]
+        Y = indecies[1, :]
 
-    return priors
+        CX = (X + 0.5) / fdim
+        CY = (Y + 0.5) / fdim
 
-# ------------------------------------------------------------------------
+        feature_prior = np.empty((1, 4))
 
-def create_prior(prior_conv : str):
-    specs = priors_specs()[0][prior_conv]
+        for aratio in aratios:
+            W = np.full((CX.shape[0], 1), scale * math.sqrt(aratio))
+            H = np.full((CX.shape[0], 1), scale / math.sqrt(aratio))
+            
+            feature_prior = np.column_stack((CX, CY, W, H))
+            all_priors = np.vstack((all_priors, feature_prior))
 
-    scale = specs['scale']
-    aspect_ratios = specs['aratios']
+    all_priors = torch.from_numpy(all_priors).clamp(0, 1)
 
-    priors = create_prior_aux(scale, aspect_ratios)
-
-    return priors
+    return all_priors
 
 
 def test():
-    priors = create_prior('conv_1')
-    print(priors)
+    all_priors = create_priors()
+    print(all_priors[-1])
 
 test()
